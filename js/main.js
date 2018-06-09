03c617d052d559e0dc658ebba127998bcc7add8c
nyatigo <sundaytigo@gmail.com>
2018-06-09 08:48:23 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,117 @@
-var models = [
+var image = document.createElement("img");
+image.src = "img/leo.png"
+image.id = "image";
+var image2 = document.createElement('img')
+image2.src = "img/cancer.jpg";
+image2.id = "image2";
+var button1 = document.createElement("button");
+button1.id = "button1"
+button1.innerText = "show"
+var button2 = document.createElement("button");
+button2.id = "button2";
+button2.innerText = "content";
 
-{
-  name:"GT350R",
-  image:"img/3.jpg",
-  price:"$64,740",
-  info:"The GT350 Track Attack is a complimentary high-performance driving experience for Shelby® GT350/GT350R owners. This one-day event is designed to build the high-performance driving skills that will help you get the most out of your new GT350/GT350R. For more information, just go to www.gt350trackattack.com or call 435-27-SPEED to register."
-  
-},
+var button3 = document.createElement("button");
+button3.innerText = " both";
+button3.id = "button3";
 
-{
-  name:"GT",
-  image:"img/5.jpg",
-  price:"$35,190",
-  info:"Move like a legend.The 2017 Ford Mustang is a high performance, sport coupe that competes with the Chevrolet Camaro and Dodge Challenger. Traditionally, the Mustang has been an enduring symbol for muscle car enthusiasts. The Shelby GT350 will be covered in a separate overview. "
-  
+var button4 = document.createElement("button");
+button4.innerText = "toggle";
+button4.id = "button4";
+
+
+
+document.body.appendChild(image);
+document.body.appendChild(image2);
+document.body.appendChild(button1);
+document.body.appendChild(button2);
+document.body.appendChild(button3);
+document.body.appendChild(button4)
+
+$("#button1").click(function(){
+	$("#image").toggle()
+})
+
+$("#button2").click(function(){
+	$("#image2").toggle()
+})
+
+$("#button3").click(function(){
+	$("img").toggle()
+})
+
+$("#button4").click(function(){
+	$("img").toggleClass("fade")
+})
+
+$(document).ready(function(){
+	$("#image").hover(function(){
+		$("#image").attr("src","img/snake.jpg");
+	},function(){
+		$("#image").attr("src","img/leo.png");
+	})
+	
+})
+
+$("#image2").hover(function(){
+	$("#image2").attr("src","img/hellboy.png");
+}, function(){
+	$("#image2").attr("src","img/cancer.jpg");
+})
+
+
+
+
+function start(){
+
+	var mai = document.createElement("main");
+document.body.appendChild(mai);
+	var d = document;
+
+	var main = d.getElementsByTagName("main")[0];
+	var ul = document.createElement("ul");
+	main.appendChild(ul);
+
+	for(i = 0; i <=4 ; i++){
+		var li = d.createElement("li");
+		li.innerText = "item" ;
+		ul.appendChild(li);
+	}
 }
 
-];
+start()
 
-function fun(){
-  var input = document.getElementById("input").value
-  var h1 = document.getElementById("name")
-  var image = document.getElementById("img")
-  var h5 = document.getElementById("price")
-  var p = document.getElementById("para")
-  for(var i = 0 ; i < models.length ; i++){
-    if(input == models[i].name){
-      h1.innerHTML = models[i].name
-      image.src= models[i].image
-      h5.innerHTML = models[i].price
-      p.innerHTML = models[i].info
-      m.innerHTML = models[i].m
+var button5 = document.createElement("button");
+button5.innerText = "button5";
+button5.id = "button5";
+document.body.appendChild(button5);
 
-    }
-  }
+$("#button5").click(function(){
+	$("ul").append( start())
+})
 
-}
+var button6 = document.createElement("button");
+button6.innerText = "button6";
+button6.id = "button6";
+document.body.appendChild(button6);
+
+$("#button6").click(function(){
+	$("body").css("backgroundColor","red")
+
+});
+
+var tittle = document.createElement("h1");
+tittle.innerText = "text";
+document.body.appendChild(tittle);
+
+$("h1").hover(function(){
+	$("h1").toggleClass("fade")
+})
+
+$("ul").hover(function(){
+	$("li").hover(function(){
+		$(this).css("color","red");
+	}, function(){
+		$(this).css("color","black");
+	})
+})
